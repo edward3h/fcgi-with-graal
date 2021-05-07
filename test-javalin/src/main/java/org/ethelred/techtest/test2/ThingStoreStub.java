e9959d010ae5547184b4e49b256a27715ec4f6c5
Edward Harman <eharman@tripadvisor.com>
2021-05-06 20:49:22 -0400

    lost track of what I was doing with this project

--- test-javalin/src/main/java/org/ethelred/techtest/test2/ThingStoreStub.java
+++ test-javalin/src/main/java/org/ethelred/techtest/test2/ThingStoreStub.java
@@ -1,37 +1,94 @@
 package org.ethelred.techtest.test2;
 
+import com.github.javafaker.Faker;
+
 import java.util.List;
 import java.util.Optional;
+import java.util.Random;
+import java.util.SortedMap;
+import java.util.TreeMap;
 
 public class ThingStoreStub implements ThingStore
 {
+    private final SortedMap<Integer, Thing> map = new TreeMap<>();
+
+    private static class ThingImpl implements Thing
+    {
+        private final int id;
+        private final String name;
+        private final String colour;
+
+        public ThingImpl(int id, String name, String colour)
+        {
+            this.id = id;
+            this.name = name;
+            this.colour = colour;
+        }
+
+        @Override
+        public int id()
+        {
+            return id;
+        }
+
+        @Override
+        public String name()
+        {
+            return name;
+        }
+
+        @Override
+        public String colour()
+        {
+            return colour;
+        }
+    }
+
+    public ThingStoreStub()
+    {
+        var faker = new Faker(new Random(1));
+        for (int i = 0; i < 10; i++)
+        {
+            createThing(new ThingImpl(0, faker.name().fullName(), faker.color().name()));
+        }
+    }
+
     @Override
     public List<Thing> getThings()
     {
-        throw new UnsupportedOperationException("ThingStoreStub.getThings");
+        return List.copyOf(map.values());
     }
 
     @Override
     public Optional<Thing> getThingById(int id)
     {
-        throw new UnsupportedOperationException("ThingStoreStub.getThingById");
+        return Optional.ofNullable(map.get(id));
     }
 
     @Override
     public Thing createThing(Thing thing)
     {
-        throw new UnsupportedOperationException("ThingStoreStub.createThing");
+        int id = map.isEmpty() ? 1 : map.lastKey() + 1;
+        var newValue = new ThingImpl(id, thing.name(), thing.colour());
+        map.put(id, newValue);
+        return newValue;
     }
 
     @Override
     public void updateThing(Thing thing)
     {
-        throw new UnsupportedOperationException("ThingStoreStub.updateThing");
+        var id = thing.id();
+        if (map.containsKey(id))
+        {
+            var newValue = new ThingImpl(id, thing.name(), thing.colour());
+            map.put(id, newValue);
+        }
     }
 
     @Override
     public void deleteThing(Thing thing)
     {
-        throw new UnsupportedOperationException("ThingStoreStub.deleteThing");
+        var id = thing.id();
+        map.remove(id);
     }
 }
